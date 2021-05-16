35283a5ba87d88baeaa676b43d54574e186e8c5c
bonmorz <2406053204@qq.com>
2021-05-16 18:13:03 +0800

    建立数据库

---
+++ app/src/main/java/com/example/exponestorage/MyDatabaseHelper.java
@@ -0,0 +1,29 @@
+package com.example.exponestorage;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.Toast;
+
+public class MyDatabaseHelper extends SQLiteOpenHelper{
+    public static final String CREATE_studentList="create table studentList("
+            +"id text primary key, "
+            +"name text,"
+            +"age text)";
+    private Context mContext;
+
+    public MyDatabaseHelper(Context context, String name,
+                            SQLiteDatabase.CursorFactory factory,int version){
+        super(context,name,factory,version);
+        mContext=context;
+    }
+    @Override
+    public void onCreate(SQLiteDatabase db){
+        db.execSQL(CREATE_studentList);
+        Toast.makeText(mContext,"Create succeeded",Toast.LENGTH_SHORT).show();
+    }
+    @Override
+    public void onUpgrade(SQLiteDatabase db,int oldVersion,int newVersion){
+
+    }
+}
